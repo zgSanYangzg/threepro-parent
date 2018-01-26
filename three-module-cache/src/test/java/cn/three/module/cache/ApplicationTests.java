039ca24b04279180fa2d7c00d60afcb1974d574b
zhougang <2582565296@qq.com>
2018-01-26 09:59:07 +0800

    修正角色模块调整位置导致的bug

--- three-module-cache/src/test/java/cn/three/module/cache/ApplicationTests.java
+++ three-module-cache/src/test/java/cn/three/module/cache/ApplicationTests.java
@@ -1,7 +1,6 @@
 package cn.three.module.cache;
 
 
-import cn.three.module.account.pojo.CAccount;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,14 +14,10 @@ public class ApplicationTests {
     @Test
     public void test() throws Exception {
 
-        CAccount a = new CAccount();
-        a.setStaffName("123张三");
-        a.setCompanyName("tae");
+        String[] arr = new String[2];
+        arr[0]="123张三";
+        arr[1]="tae";
         Redis.set("213","213");
-        Redis.setSingle(a,"company","group","organization");
         System.out.println(Redis.get("213").toString()+"=========================");
-        CAccount b =Redis.getSingle(CAccount.class,"company","group","organization");
-        System.out.print(b.getStaffName());
     }
-
 }
